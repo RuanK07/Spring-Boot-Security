a0d7d088569851feeb04320e7f9075cb8694a477
Ruan Kennedy <Ruank07@gmail.com>
2023-02-17 11:41:56 -0300

    Basic Auth

---
+++ src/main/java/com/workshop/ruan/RuanApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.ruan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuanApplication.class, args);
+	}
+
+}
